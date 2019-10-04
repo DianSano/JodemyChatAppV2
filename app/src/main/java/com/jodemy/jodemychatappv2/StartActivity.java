f767de01c4464b81ae58ccaf0fb5a0073a9bfbde
DianSano <alb.vds@gmail.com>
2019-10-04 21:37:43 +0700

    video 7 - update if empty_fields

--- app/src/main/java/com/jodemy/jodemychatappv2/StartActivity.java
+++ app/src/main/java/com/jodemy/jodemychatappv2/StartActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
-    private Button mRegBtn;
+    private Button mRegBtn, mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,17 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         mRegBtn = findViewById(R.id.start_reg_btn);
+        mLoginBtn = findViewById(R.id.start_login_btn);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login_intent = new Intent(StartActivity.this,
+                        LoginActivity.class);
+                startActivity(login_intent);
+            }
+        });
+
 
         mRegBtn.setOnClickListener(new View.OnClickListener() {
             @Override
