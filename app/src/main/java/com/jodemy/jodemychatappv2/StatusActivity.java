b90a8a5b2cda9dc6ed887f023a701a986d482062
DianSano <alb.vds@gmail.com>
2019-10-11 21:22:42 +0700

    StatusActivity.java initial commit

---
+++ app/src/main/java/com/jodemy/jodemychatappv2/StatusActivity.java
@@ -0,0 +1,90 @@
+package com.jodemy.jodemychatappv2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StatusActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    private TextInputLayout mStatus;
+    private Button mSavebtn;
+
+    //Firebase
+    private DatabaseReference mStatusDatabase;
+    private FirebaseUser mCurrentUser;
+
+    //progress dialog
+    private ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        //firebase
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String current_uid = mCurrentUser.getUid();
+        mStatusDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mToolbar = findViewById(R.id.status_appBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Account Status");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String status_value = getIntent().getStringExtra("status_value");
+
+        mStatus = findViewById(R.id.status_input);
+        mSavebtn = findViewById(R.id.status_save_btn);
+
+        mStatus.getEditText().setText(status_value);
+
+        mSavebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Progress
+                mProgress = new ProgressDialog(StatusActivity.this);
+                mProgress.setTitle("Saving Changes");
+                mProgress.setMessage("Please wait while we save the changes");
+                mProgress.show();
+
+                String status = mStatus.getEditText().getText().toString();
+
+                mStatusDatabase.child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()) {
+                            mProgress.dismiss();
+                            Intent status_intent = new Intent(StatusActivity.this,
+                                    SettngsActivity.class);
+                            startActivity(status_intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    "There was some error in saving changes",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+}
